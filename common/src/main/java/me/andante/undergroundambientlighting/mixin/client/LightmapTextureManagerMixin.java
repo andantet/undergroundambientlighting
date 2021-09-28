ace3f5dd412518620682753792d5105e4110576b
Daniel Orr <orrdaniel73@gmail.com>
2021-09-29 00:44:33 +0100

    update to forge 1.17

--- common/src/main/java/me/andante/undergroundambientlighting/mixin/client/LightmapTextureManagerMixin.java
+++ common/src/main/java/me/andante/undergroundambientlighting/mixin/client/LightmapTextureManagerMixin.java
@@ -5,33 +5,34 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(LightmapTextureManager.class)
 public abstract class LightmapTextureManagerMixin {
     @Shadow @Final private MinecraftClient client;
 
-    @ModifyVariable(method = "update", ordinal = 5, at = @At(value = "STORE", ordinal = 0))
-    private float applyUndergroundAmbientLight(float original) {
-        if (UALClient.isEnabled()) {
-            assert this.client.player != null;
+    @Inject(method = "getBrightness", at = @At("RETURN"), cancellable = true)
+    private void applyUndergroundAmbientLight(World world, int lightLevel, CallbackInfoReturnable<Float> cir) {
+        if (this.client.player != null && UALClient.isEnabled() && lightLevel <= 1) {
             double playerY = this.client.player.getY();
             int startY = 63;
 
             if (playerY <= startY) {
                 int endY = 32;
                 float addedLight = 0.038f;
-                original += playerY >= endY
-                    ? addedLight * (float) ((Math.cos((playerY - endY) / (Math.abs(startY - endY) / Math.PI)) + 1) / 2)
-                    : addedLight;
+                cir.setReturnValue(cir.getReturnValueF() +
+                    playerY >= endY
+                        ? addedLight * (float) ((Math.cos((playerY - endY) / (Math.abs(startY - endY) / Math.PI)) + 1) / 2)
+                        : addedLight
+                );
             }
         }
-
-        return original;
     }
 }
--- forge/src/main/java/me/andante/undergroundambientlighting/forge/client/UALClientForge.java
+++ forge/src/main/java/me/andante/undergroundambientlighting/forge/client/UALClientForge.java
@@ -5,6 +5,8 @@ import me.andante.undergroundambientlighting.client.UALClient;
 import me.andante.undergroundambientlighting.client.platform.AbstractClientPlatform;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,11 +14,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fmlclient.registry.ClientRegistry;
 import org.lwjgl.glfw.GLFW;
 
+@OnlyIn(Dist.CLIENT)
 public class UALClientForge implements AbstractClientPlatform {
     private static final KeyBinding keyBinding = new KeyBinding(
-            "key." + UndergroundAmbientLighting.MOD_ID + "." + UndergroundAmbientLighting.MOD_ID,
-            GLFW.GLFW_KEY_I,
-            "key.category." + UndergroundAmbientLighting.MOD_ID
+        String.format("key.%s.%s", UndergroundAmbientLighting.MOD_ID, UndergroundAmbientLighting.MOD_ID),
+        GLFW.GLFW_KEY_I,
+        String.format("key.category.%s", UndergroundAmbientLighting.MOD_ID)
     );
 
     private final UALClient core;
